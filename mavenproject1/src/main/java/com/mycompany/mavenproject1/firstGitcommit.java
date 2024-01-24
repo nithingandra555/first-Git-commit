44f5ea6110cd3c1813ad663322e76ba0c25f9fdc
Nithin Gandra <112127344+nithingandra555@users.noreply.github.com>
2024-01-24 17:48:41 -0600

    Create firstGitcommit.java

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/firstGitcommit.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mavenproject1;
+
+/**
+ *
+ * @author S555929
+ */
+public class firstGitcommit {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+       
+    }
+}
