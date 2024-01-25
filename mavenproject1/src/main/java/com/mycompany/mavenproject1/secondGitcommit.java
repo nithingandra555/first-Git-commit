e3dc303077ce4f7133a295c4fca9871d033d0773
Nithin Gandra <112127344+nithingandra555@users.noreply.github.com>
2024-01-24 19:38:54 -0600

    second git commit

--- mavenproject1/src/main/java/com/mycompany/mavenproject1/secondGitcommit.java
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/secondGitcommit.java
@@ -1,20 +1,21 @@
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 package com.mycompany.mavenproject1;
 
 /**
  *
  * @author S555929
  */
-public class firstGitcommit {
-
-    public static void main(String[] args) {
-        System.out.println("Hello World. I am coming for you!");
+public class secondGitcommit {
+     public static void main(String[] args) {
+        System.out.println("Hello World!");
         
         
         
        
     }
 }
+    
+
